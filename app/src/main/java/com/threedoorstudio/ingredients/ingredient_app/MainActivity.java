c63cc4d9da5de64cf5d5b251bcb5aacdb0006fde
casparmkerr <casparmkerr@gmail.com>
2017-07-14 19:28:31 +0200

    Finishing first tutorial hopefully https://www.youtube.com/watch?v=D423T5JVF-s&list=PL9jCwTXYWjDJUJATHM0Lrjk9N5n6QZqBg&index=2

--- app/src/main/java/com/threedoorstudio/ingredients/ingredient_app/MainActivity.java
+++ app/src/main/java/com/threedoorstudio/ingredients/ingredient_app/MainActivity.java
@@ -1,17 +1,27 @@
 package com.threedoorstudio.ingredients.ingredient_app;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
+/*
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager; */
+import android.hardware.camera2.*;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.LruCache;
 import android.util.Size;
 import android.view.TextureView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,12 +38,15 @@ public class MainActivity extends AppCompatActivity {
     private TextureView mTextureView;
     private Size mPreviewSize;
     private String mCameraId;
+    private int CAMERA_PERMISSION;
 
     private TextureView.SurfaceTextureListener mSurfaceTextureListener =
             new TextureView.SurfaceTextureListener() {
                 @Override
                 public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
                     setupCamera(width, height);
+                    openCamera();
+
                 }
 
                 @Override
@@ -48,6 +61,34 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+                }
+            };
+
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback =
+            new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(CameraDevice camera) {
+                    mCameraDevice = camera;
+                    Toast.makeText(getApplicationContext(), "CameraDevice Opened", Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void onDisconnected(CameraDevice camera) {
+                    camera.close();
+                    mCameraDevice = null;
+
+                }
+
+                @Override
+                public void onError(CameraDevice camera, int error) {
+                    camera.close();
+                    mCameraDevice = null;
+                    Toast.makeText(getApplicationContext(), "That's an error!", Toast.LENGTH_LONG).show();
+
+
 
                 }
             };
@@ -71,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setupCamera( int width, int height) {
+    private void setupCamera(int width, int height) {
         CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             for(String cameraId : cameraManager.getCameraIdList()) {
@@ -86,8 +127,6 @@ public class MainActivity extends AppCompatActivity {
                 return;
 
             }
-
-
         } catch (CameraAccessException e) {
             e.printStackTrace();
 
@@ -118,5 +157,56 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         return mapSizes[0];
+    }
+
+    private void openCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        while (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+            Toast.makeText(getApplicationContext(), "No Permission!", Toast.LENGTH_SHORT).show();
+
+        }
+        try {
+
+            cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, null); //This can be optimised by running it on a separate thread, but that comes later
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+/*    //Requesting permission
+    private void requestStoragePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)){
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},STORAGE_PERMISSION_CODE);
+    }
+*/
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if(requestCode == CAMERA_PERMISSION){
+
+            //If permission is granted
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                //Displaying a toast
+                Toast.makeText(this,"Permission granted, now you can use the app",Toast.LENGTH_LONG).show();
+            }else{
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
